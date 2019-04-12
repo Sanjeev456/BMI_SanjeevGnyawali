0fa379190a72fbf3c59568e6345cccc8a61c7c1d
sanjeev <gnyawali.sanjeev@gmail.com>
2019-04-12 20:35:00 +0545

    Initial commit

---
+++ app/src/main/java/com/example/bmi_sanjeevgnyawali/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.bmi_sanjeevgnyawali;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etHeight, etWeight;
+    private TextView tvOutput;
+    private Button btnCompute;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etHeight = findViewById(R.id.etHeight);
+        etWeight = findViewById(R.id.etWeight);
+        btnCompute = findViewById(R.id.btnCompute);
+        tvOutput = findViewById(R.id.tvOutput);
+
+        btnCompute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double height = Double.parseDouble(etHeight.getText().toString());
+                double weight = Double.parseDouble(etWeight.getText().toString());
+                BMICalculate bmi = new BMICalculate();
+                bmi.setHeight(height);
+                bmi.setWeight(weight);
+                double res = bmi.BMI();
+
+                tvOutput.setText(Double.toString(res) + "  kg/m^2");
+
+                if(res<18.5){
+                    Toast.makeText(MainActivity.this,"Underweight",Toast.LENGTH_LONG).show();
+                }else if(res>=18.5 && res<=24.9){
+                    Toast.makeText(MainActivity.this,"Normalweight",Toast.LENGTH_LONG).show();
+                }else if(res>=25 && res<=29.9){
+                    Toast.makeText(MainActivity.this,"Overweight",Toast.LENGTH_LONG).show();
+                }else if(res>=30){
+                    Toast.makeText(MainActivity.this,"Obesity",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
+
